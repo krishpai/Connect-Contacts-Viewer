f5dd2c8dc7bb514bdd969e84e75b52f6b1d15650
Krish Pai <krish.pai@outlook.com>
2026-04-04 21:28:11 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useEffect, useState, useCallback, useRef} from "react";
+import { useEffect, useState, useCallback} from "react";
 import { MsalAuthenticationTemplate } from "@azure/msal-react";
-import { AmazonConnectApp  } from '@amazon-connect/app';
-import { AgentClient } from "@amazon-connect/contact";
 import { PageLayout } from "./components/PageLayout";
 import { SearchBox } from "./components/SearchBox";
 import { SearchResultsView } from "./components/SearchResultsView";
@@ -15,33 +13,20 @@ import "./App.css";
 
 
 const API_ENDPOINT_ENTRA_AUTH = import.meta.env.VITE_API_URL_ENTRA_AUTH;
-const API_ENDPOINT_CONNECT_AUTH = import.meta.env.VITE_API_URL_CONNECT_AUTH;
+
 const API_SCOPE = import.meta.env.VITE_API_SCOPE;
 const isIframe = window.self !== window.top; // Immediate check
 
-// Helper to check if we are in an MSAL "hidden" frame
-const isMsalInternalFrame = window.location.hash.includes("code=") || 
-                             window.location.hash.includes("error=") ||
-                             window.name.includes("msal");
 
 function App() {
   const { instance, accounts } = useMsal();
 
-  // SDK & Clients State
-  const [sdkInitialized, setSdkInitialized] = useState<boolean>(false);
-  const [, setAgentClient] = useState<AgentClient | null>(null);
-  const [, setConnectProvider] = useState<AmazonConnectApp| null>(null);
-  
   // Business State
   const [region, setRegion] = useState("");
   const [userName, setUserName] = useState<string |null|undefined>("");
   const [searchResult, setSearchResult] = useState("");
   const [loading, setLoading] = useState<boolean>(false);
-  const [, setConnectUserId] = useState<string | null>(null);
   
-
-  // Refs to prevent double-init or stale closures
-  const sdkStarted = useRef(false);
 
   const acquireTokenWithRecovery = useAcquireTokenWithRecovery();
 
@@ -98,55 +83,11 @@ function App() {
     // Include all stable dependencies
   }, [ acquireTokenWithRecovery]);
   
-  /**
-   * Fetches the user region from the backend API for iframe embedded app.
-   */
-  const getUserInfo_Connect = useCallback(async (connectUserId: string|null) => {
-    console.log("*********** in getUserRegion_Connect");
-    //connectUserId = "79e4e9fe-40f7-44d1-969e-d82113792b2f";
-    const apiUrl = `${API_ENDPOINT_CONNECT_AUTH}?function_code=get_user_info&AgentUserId=${connectUserId}`;
-    console.log('apiUrl: ', apiUrl)
-    try
-    {
-      const response = await fetch(apiUrl, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (!response.ok) 
-      {
-        throw new Error(`API error: ${response.status} ${response.statusText}`);
-      }
-
-      const data = await response.json();
-      if (data?.success && data?.found) 
-      {
-        setRegion(data.region);
-        setUserName(data.userName);
-
-        console.log("User name identified:", data.userName);
-        console.log("User region identified:", data.region);
-      }
-      else
-      {
-        throw new Error(`API error: ${response.status} ${response.statusText}`);
-      }
-    }
-    catch (error) 
-    {
-      console.log('error: ', error)
-      setRegion("ALL");
-      setUserName("Unknown user");
-    }
-
-  }, [])     
-
+  
   useEffect(() => {
     
     // 1. Standalone logic
-    if (!isIframe && accounts.length > 0) 
+    if (accounts.length > 0) 
     {
       console.info("In Standalone logic");
       instance.setActiveAccount(accounts[0]);
@@ -160,60 +101,10 @@ function App() {
       getUserInfo_Entra(username);
     }
 
-    if (isMsalInternalFrame) return;
-
-    // 2. Iframe / Amazon Connect logic
-    if (isIframe && !sdkStarted.current) 
-    {
-      console.info("In Iframe logic");
-      sdkStarted.current = true; // Guard against React 18 double-run
-      
-      const amazonConnectApp =  AmazonConnectApp.init({
-        onCreate: async (event) => {
-          setSdkInitialized(true); // Handshake complete
-          console.log('************ App initialized with context:', event.context);
-          
-          // Create an Agent Client using the provider
-          const agentClient = new AgentClient({ provider: amazonConnectApp.provider });
-
-          setAgentClient(agentClient);
-
-          const agentARN = await agentClient.getARN();
-          // Extract user ID from ARN
-          // ARN format: arn:aws:connect:region:account:instance/instance-id/agent/user-id
-          const userIdMatch = agentARN.match(/\/agent\/(.+)$/);
-          const connectUserId = userIdMatch ? userIdMatch[1] : null;
-          console.log("User ID:", connectUserId);
-          console.log("Agent ARN:", agentARN);
-          
-          setConnectUserId(connectUserId);
-          setLoading(false);
-
-          if (connectUserId) 
-          {
-            getUserInfo_Connect(connectUserId);
-          }
-        },
-        onDestroy: async (event) => {
-          console.log('App being destroyed:', event);
-        },
-      });
-
-        // Save the provider to state so you can use it globally in your app
-      setConnectProvider(amazonConnectApp.provider);     
-    };
-  }, [accounts, instance, getUserInfo_Entra, getUserInfo_Connect, accounts.length]);
+   
+  }, [accounts, instance, getUserInfo_Entra, , accounts.length]);
 
   
-  
-
-  // If we are in an iframe but the SDK hasn't finished its handshake yet,
-  // we show a neutral loading screen to prevent the MSAL Redirect from firing.
-  if (isIframe && !sdkInitialized) 
-  {
-      return <p>Connecting to Agent Workspace...</p>;
-  }
-
   // Main UI Fragment to keep code DRY
   const renderMainContent = () => (
     <PageLayout userName={userName ?? "User"}  region={region}>
